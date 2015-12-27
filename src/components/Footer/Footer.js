0563983df0bfb71f506ca5738665d40ac1881771
James Lloyd <james.jd.lloyd@gmail.com>
2015-12-27 16:35:19 -0600

    remove unused components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -23,8 +23,6 @@ class Footer extends Component {
           <span className={s.spacer}>·</span>
           <a className={s.link} href="/" onClick={Link.handleClick}>Home</a>
           <span className={s.spacer}>·</span>
-          <a className={s.link} href="/privacy" onClick={Link.handleClick}>Privacy</a>
-          <span className={s.spacer}>·</span>
           <a className={s.link} href="/not-found" onClick={Link.handleClick}>Not Found</a>
         </div>
       </div>
