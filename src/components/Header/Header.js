442275d93b57fef6d76d66e5822c5708f3cdff2f
James Lloyd <james.jd.lloyd@gmail.com>
2015-12-27 15:28:18 -0600

    change branding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,11 +23,11 @@ class Header extends Component {
           <Navigation className={s.nav} />
           <a className={s.brand} href="/" onClick={Link.handleClick}>
             <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>Three Ovals Around a Circle</span>
           </a>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made complicated</p>
+            <h1 className={s.bannerTitle}>FHIR Test App</h1>
+            <p className={s.bannerDesc}>A Patient Search Demo App</p>
           </div>
         </div>
       </div>
